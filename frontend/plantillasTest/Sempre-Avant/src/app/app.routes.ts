e2abe03f0f93a0859cd060de78666f7780b4542f
HerreraGenerEmilio <herrerageneremilio@gmail.com>
2024-02-13 09:39:52 +0100

    view home creada

--- frontend/plantillasTest/Sempre-Avant/src/app/app.routes.ts
+++ frontend/plantillasTest/Sempre-Avant/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', component: HomeComponent}
+];
