2380059712f42907eaca817d0fe89ddbc2b0f7de
HerreraGenerEmilio <herrerageneremilio@gmail.com>
2024-02-13 10:35:49 +0100

    enrutado establecido

--- frontend/plantillasTest/Sempre-Avant/src/app/app.routes.ts
+++ frontend/plantillasTest/Sempre-Avant/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { TestComponent } from './views/test/test.component';
 
 export const routes: Routes = [
     { path: 'home', redirectTo: 'home', pathMatch: 'full' },
-    { path: '', component: HomeComponent}
+    { path: '', component: HomeComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'test', component: TestComponent}
+    
 ];
