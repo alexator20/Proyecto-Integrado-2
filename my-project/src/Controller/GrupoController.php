97339a666efcec9098c008440738ae0f67bb39d7
HerreraGenerEmilio <herrerageneremilio@gmail.com>
2024-02-14 13:09:38 +0100

    pruebas enrutado, comprobación listado para verificar relaciones symfony correctas

--- my-project/src/Controller/GrupoController.php
+++ my-project/src/Controller/GrupoController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\RelGrupoEvento;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class GrupoController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
@@ -17,8 +25,11 @@ class GrupoController extends AbstractController
     #[Route('/grupo', name: 'app_grupo')]
     public function index(): Response
     {
+        $eventoGrupoRepository = $this->em->getRepository(RelGrupoEvento::class)->findAll();
+
         return $this->render('grupo/index.html.twig', [
             'controller_name' => 'GrupoController',
+            'resultados' => $eventoGrupoRepository
         ]);
     }
 }
