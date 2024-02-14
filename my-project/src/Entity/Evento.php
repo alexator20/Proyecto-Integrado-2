d44798819de56bf786eb51f28ec77053bb52a01d
DavitCostaMartinez <92036332+DavitCostaMartinez@users.noreply.github.com>
2024-02-14 09:29:33 +0100

    tabla de eventos

---
+++ my-project/src/Entity/Evento.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventoRepository::class)]
+class Evento
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fecha = null;
+
+    #[ORM\Column]
+    private ?int $numeroparticipantes = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): static
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getNumeroparticipantes(): ?int
+    {
+        return $this->numeroparticipantes;
+    }
+
+    public function setNumeroparticipantes(int $numeroparticipantes): static
+    {
+        $this->numeroparticipantes = $numeroparticipantes;
+
+        return $this;
+    }
+}
