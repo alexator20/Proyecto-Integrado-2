a61bd2fdbdc657a857f69e43df9abae7c4e49b0d
alexator20 <90261293+alexator20@users.noreply.github.com>
2024-02-14 08:28:37 +0100

    jumbotron component

--- frontend/plantillasTest/Sempre-Avant/src/app/app.component.ts
+++ frontend/plantillasTest/Sempre-Avant/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, JumbotronComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
