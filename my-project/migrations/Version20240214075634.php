994361aedb117586bdd0c6ac844ded1c149bc89b
TameHub <fernandolopeztame@gmail.com>
2024-02-14 08:59:22 +0100

    Creacion usuarios
    Añadido de la tabla "usuario" la cual tiene relacion con "grupo"

--- my-project/migrations/Version20240214075634.php
+++ my-project/migrations/Version20240214075634.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240213092728 extends AbstractMigration
+final class Version20240214075634 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,15 @@ final class Version20240213092728 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE noticia (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, descripcion LONGTEXT NOT NULL, fecha DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE usuario (id INT AUTO_INCREMENT NOT NULL, grupo_perteneciente_id INT NOT NULL, nombre VARCHAR(255) NOT NULL, edad INT DEFAULT NULL, rol VARCHAR(255) NOT NULL, INDEX IDX_2265B05DC011DA75 (grupo_perteneciente_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE usuario ADD CONSTRAINT FK_2265B05DC011DA75 FOREIGN KEY (grupo_perteneciente_id) REFERENCES grupo (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE usuario DROP FOREIGN KEY FK_2265B05DC011DA75');
         $this->addSql('DROP TABLE noticia');
+        $this->addSql('DROP TABLE usuario');
     }
 }
--- my-project/src/Entity/Grupo.php
+++ my-project/src/Entity/Grupo.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GrupoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GrupoRepository::class)]
@@ -18,6 +20,14 @@ class Grupo
 
     #[ORM\Column(length: 255)]
     private ?string $seccion = null;
+
+    #[ORM\OneToMany(targetEntity: Usuario::class, mappedBy: 'grupo_perteneciente')]
+    private Collection $usuarios;
+
+    public function __construct()
+    {
+        $this->usuarios = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +54,36 @@ class Grupo
     public function setSeccion(string $seccion): static
     {
         $this->seccion = $seccion;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Usuario>
+     */
+    public function getUsuarios(): Collection
+    {
+        return $this->usuarios;
+    }
+
+    public function addUsuario(Usuario $usuario): static
+    {
+        if (!$this->usuarios->contains($usuario)) {
+            $this->usuarios->add($usuario);
+            $usuario->setGrupoPerteneciente($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUsuario(Usuario $usuario): static
+    {
+        if ($this->usuarios->removeElement($usuario)) {
+            // set the owning side to null (unless already changed)
+            if ($usuario->getGrupoPerteneciente() === $this) {
+                $usuario->setGrupoPerteneciente(null);
+            }
+        }
 
         return $this;
     }
