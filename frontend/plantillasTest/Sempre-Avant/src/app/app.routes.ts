16042217aa99811791516939859778c36f2f699e
TameHub <fernandolopeztame@gmail.com>
2024-02-19 09:15:18 +0100

    Completado de Baden Powell/ Añadido de Cronologia

--- frontend/plantillasTest/Sempre-Avant/src/app/app.routes.ts
+++ frontend/plantillasTest/Sempre-Avant/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { EscultaComponent } from './views/esculta/esculta.component';
 import { ClanComponent } from './views/clan/clan.component';
 import { ScoutersComponent } from './views/scouters/scouters.component';
 import { BadenPowellComponent } from './views/baden-powell/baden-powell.component';
+import { CronologiaComponent } from './views/cronologia/cronologia.component';
 
 export const routes: Routes = [
     { path: 'home', redirectTo: 'home', pathMatch: 'full' },
@@ -20,6 +21,7 @@ export const routes: Routes = [
     { path: 'esculta', component: EscultaComponent},
     { path: 'clan', component: ClanComponent},
     { path: 'scouters', component: ScoutersComponent},
-    { path: 'badenpowell', component: BadenPowellComponent}
+    { path: 'badenpowell', component: BadenPowellComponent},
+    { path: 'cronologia', component: CronologiaComponent}
 ];
 
---
+++ frontend/plantillasTest/Sempre-Avant/src/app/views/cronologia/cronologia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cronologia',
+  standalone: true,
+  imports: [],
+  templateUrl: './cronologia.component.html',
+  styleUrl: './cronologia.component.css'
+})
+export class CronologiaComponent {
+
+}
