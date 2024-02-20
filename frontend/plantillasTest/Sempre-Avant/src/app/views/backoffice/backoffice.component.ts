24ae26acb777b97e68deea1e0ab34c0abd322a77
HerreraGenerEmilio <herrerageneremilio@gmail.com>
2024-02-20 13:44:30 +0100

    backoffice diseñado y medio funcionando

--- frontend/plantillasTest/Sempre-Avant/src/app/views/backoffice/backoffice.component.ts
+++ frontend/plantillasTest/Sempre-Avant/src/app/views/backoffice/backoffice.component.ts
@@ -10,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class BackofficeComponent {
 
+  public insertUser(): void {
+    // Logic to insert a user goes here
+    alert('insert')
+    // You should write the logic to insert a user in your application.
+    // This could involve making HTTP requests, interacting with a database, etc.
+  }
+  public updateUser(): void {
+    // Logic to insert a user goes here
+    alert('update')
+    // You should write the logic to insert a user in your application.
+    // This could involve making HTTP requests, interacting with a database, etc.
+  }
+  public deleteUser(): void {
+    // Logic to insert a user goes here
+    alert('eliminar')
+    // You should write the logic to insert a user in your application.
+    // This could involve making HTTP requests, interacting with a database, etc.
+  }
 }
