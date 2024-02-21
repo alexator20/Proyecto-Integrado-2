c7f50f1d917ad36d6aab7fcef73a1dcf29e2cbdb
alexator20 <90261293+alexator20@users.noreply.github.com>
2024-02-21 10:13:15 +0100

    vista fotos

---
+++ frontend/plantillasTest/Sempre-Avant/src/app/views/fotos/fotos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FotosComponent } from './fotos.component';
+
+describe('FotosComponent', () => {
+  let component: FotosComponent;
+  let fixture: ComponentFixture<FotosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FotosComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FotosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/plantillasTest/Sempre-Avant/src/app/views/fotos/fotos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fotos',
+  standalone: true,
+  imports: [],
+  templateUrl: './fotos.component.html',
+  styleUrl: './fotos.component.css'
+})
+export class FotosComponent {
+
+}
