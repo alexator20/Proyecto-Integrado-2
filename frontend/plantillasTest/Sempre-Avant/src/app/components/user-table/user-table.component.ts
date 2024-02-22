31403d29f107c768e966a5e834ff1e32ff48e7af
HerreraGenerEmilio <herrerageneremilio@gmail.com>
2024-02-22 08:48:35 +0100

    componente listar usuarios

---
+++ frontend/plantillasTest/Sempre-Avant/src/app/components/user-table/user-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-table.component.html',
+  styleUrl: './user-table.component.css'
+})
+export class UserTableComponent {
+
+}
