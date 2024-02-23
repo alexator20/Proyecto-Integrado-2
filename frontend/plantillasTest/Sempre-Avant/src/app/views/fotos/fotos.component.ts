c557184f8e96e560f663b32106818dc14d4e6c3f
alexator20 <90261293+alexator20@users.noreply.github.com>
2024-02-23 12:24:09 +0100

    intento de fotos

--- frontend/plantillasTest/Sempre-Avant/src/app/views/fotos/fotos.component.ts
+++ frontend/plantillasTest/Sempre-Avant/src/app/views/fotos/fotos.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-fotos',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './fotos.component.html',
   styleUrl: './fotos.component.css'
 })
 export class FotosComponent {
 
+  reactiveForm = new FormGroup({
+    routes: new FormControl(''),
+  });
+
+  public rutas: { src: any; title: string }[] = [
+    { src: '../../../assets/grupo1/2014_fallas', title: 'grupo1' },
+    { src: 'ruta', title: 'grupo2' },
+    { src: 'ruta', title: 'grupo3' },
+    { src: 'ruta', title: 'grupo4' },
+  ]
+
+  public onFolder(index: number) {
+    //console.log(this.reactiveForm.value.routes[index])
+  }
+
+  public onSubmit(): void {
+    console.log('form: ', this.reactiveForm.value.routes);
+  }
 }
